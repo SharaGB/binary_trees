d2901b661f5c0633387725835607b7a4af12af67
daniel <danydu1299@gmail.com>
2022-03-22 13:40:42 -0500

    Node left mean that doesn't have childs

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf(If have childs)
+ *
+ * @node: Node to check if have childs
+ * Return:  1 if node is a leaf, otherwise 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
+	else
+		return (1);
+}

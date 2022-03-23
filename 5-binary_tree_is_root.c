fae338987c29bbb39bd3bedf1cff8a709d3c98e7
SharaGB <shara.garcia.betancur@gmail.com>
2022-03-23 00:42:57 -0500

    Fix return if node is not root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,7 +10,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node->parent == NULL)
 		return (1);
-	else if (node == NULL || node->parent != NULL)
+	else if (node == NULL && node->parent != NULL)
 		return (0);
 	return (0);
 }

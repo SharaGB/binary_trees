81aaea92bc26eee8800710777ec19526a55b5b67
daniel <danydu1299@gmail.com>
2022-03-23 10:32:36 -0500

    Fix binary tree leaves, but is right? A node without children has one leave?, help for the function that check if a node if leaf

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,7 +11,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (binary_tree_is_leaf(tree))
+	if (binary_tree_is_leaf(tree) == 1)
 		return (1);
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
